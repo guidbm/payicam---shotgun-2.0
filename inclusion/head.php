f408d58fbf0f3df71c508668d12b917a8d97e3aa
Guillaume Dubois <dbmgui@gmail.com>
2017-10-10 11:54:58 +0200

    mise en forme header

--- inclusion/head.php
+++ inclusion/head.php
@@ -2,9 +2,10 @@
 <html>
 	<head>
 		<meta charset="utf-8"/>
-		<title>Gala Icam</title>
-		<meta name="description" content="Site de présentation du gala">
+		<title>Shotgun</title>
+		<meta name="description" content="ShotgunPayicam">
 		<link href="style/css/bootstrap.css" rel="stylesheet">
+		<link rel="stylesheet" href="style/css/jqueryui/style.css">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     	<meta name="viewport" content="width=device-width, initial-scale=1">
--- inclusion/header.php
+++ inclusion/header.php
@@ -18,28 +18,26 @@
     <![endif]-->
 
   </head>
-  <body>
-    <nav class="navbar navbar-fixed-top navbar-inverse">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span><!-- 
-          </button>
-          <a class="navbar-brand" href=""><img src="img/PayIcam-h30-white.png"></a>
-        </div> -->
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="about.php">A propos</a></li>
-            <li>FAQ & Tutos</a></li>
-            <li><a href="contact.php">Contact</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="logout.php">Déconnexion</a></li>
-          </ul>
-        </div><!-- /.nav-collapse -->
-      </div><!-- /.container -->
-    </nav><!-- /.navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Payicam</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only"></span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">A propos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">FAQ & Tutos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+</html>
