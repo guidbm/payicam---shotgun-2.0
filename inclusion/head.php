df080e038efeb8b36733e1bce353ea304af70ab2
Unknown <gregoire.dervaux@2019.icam.fr>
2017-10-10 11:53:47 +0200

    test sur le css pour le footer

--- inclusion/head.php
+++ inclusion/head.php
@@ -5,6 +5,7 @@
 		<title>Gala Icam</title>
 		<meta name="description" content="Site de présentation du gala">
 		<link href="style/css/bootstrap.css" rel="stylesheet">
+		<link href="style/css/style.css" rel="stylesheet">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     	<meta name="viewport" content="width=device-width, initial-scale=1">
