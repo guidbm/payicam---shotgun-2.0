87c039867b59ec4579753c1ad5ccd84f1ac27737
Unknown <gregoire.dervaux@2019.icam.fr>
2017-10-10 11:29:58 +0200

    1er ebauche head

---
+++ inclusion/head.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>Gala Icam</title>
+		<meta name="description" content="Site de présentation du gala">
+		<link href="style/css/bootstrap.css" rel="stylesheet">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+</html>
