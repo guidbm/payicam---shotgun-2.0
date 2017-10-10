0487cbae931023aa4db3266222f09ef0cd486909
Unknown <gregoire.dervaux@2019.icam.fr>
2017-10-10 11:12:02 +0200

    1er ebauche index

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-index.php
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php include("inclusion/head.php");?>
+	</head>
+
+	<body>
+		<header>
+			<?php include("inclusion/header.php");?>
+		</header>
+
+		<section>
+		
+		</section>
+
+		<footer>
+			<?php include("inclusion/php");?>
+		</footer>
+	</body>
+</html>
